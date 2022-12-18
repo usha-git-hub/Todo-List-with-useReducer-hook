e1da94dd606b7e349d60fece9ef13ec3ca874389
usha-git-hub <113238878+usha-git-hub@users.noreply.github.com>
2022-12-18 20:06:53 +0530

    Update todo.js

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,20 +1,4 @@
-import React from "react";
-const Todo = ({ id, value, dispatch }) => {
-  return (
-    <div className="todo">
-      <div className="todo-title">{value}   <button
-        className="todo-delete"
-        onClick={() => dispatch({ type: "delete_todo", payload: { id: id } })}
-      >
-        Delete
-      </button></div>
-   
-    </div>
-  );
-};
-
-export { Todo };
-[7:26 pm, 18/12/2022] Satyesh Newton: const todoReducer = (state, action) => {
+const todoReducer = (state, action) => {
   switch (action.type) {
     case "add_todo":
       return [...state, newTodo(action.payload.title)];
