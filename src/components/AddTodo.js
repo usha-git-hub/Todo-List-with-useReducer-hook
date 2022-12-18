f1c08c273bdd847de377818bd5e796b95fb41bc4
usha-git-hub <113238878+usha-git-hub@users.noreply.github.com>
2022-12-18 19:51:36 +0530

    Update AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,4 +1,4 @@
-[7:25 pm, 18/12/2022] Satyesh Newton: import React, {  useState } from "react";
+ import React, {  useState } from "react";
 
 const AddTodo = ({ dispatch }) => {
 
@@ -30,7 +30,29 @@ const AddTodo = ({ dispatch }) => {
         />
           e.preventDefault();
         <button type="submit">Add</button>
-       …
+          dispatch({ type: "add_todo", payload: { title: todo } });
+      </form>
+          setTodo("");
+    </>
+        }}
+  );
+      >
+};
+        <input
+
+          id="todo-input"
+export { AddTodo };
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+    </>
+  );
+};
+
+export { AddTodo };
 [7:25 pm, 18/12/2022] Satyesh Newton: import React, { useReducer } from "react";
 import { todoReducer } from "../reducers/todo";
 import "../styles/App.css";
@@ -56,3 +78,36 @@ const App = () => {
   );
 };
 
+export default App;
+[7:25 pm, 18/12/2022] Satyesh Newton: import React from "react";
+const Todo = ({ id, value, dispatch }) => {
+  return (
+    <div className="todo">
+      <div className="todo-title">{value}   <button
+        className="todo-delete"
+        onClick={() => dispatch({ type: "delete_todo", payload: { id: id } })}
+      >
+        Delete
+      </button></div>
+   
+    </div>
+  );
+};
+
+export { Todo };
+[7:26 pm, 18/12/2022] Satyesh Newton: const todoReducer = (state, action) => {
+  switch (action.type) {
+    case "add_todo":
+      return [...state, newTodo(action.payload.title)];
+    case "delete_todo":
+      return state.filter((task) => task.id !== action.payload.id);
+    default:
+      return state;
+  }
+};
+
+function newTodo(task) {
+  return { id: Date.now(), title: task };
+}
+
+export { todoReducer };
