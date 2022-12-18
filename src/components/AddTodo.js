fe221ccc8f15956eb2efafdb40641e6ae5e1b680
usha-git-hub <113238878+usha-git-hub@users.noreply.github.com>
2022-12-18 20:04:57 +0530

    Update AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,44 +1,26 @@
-import React, {  useState } from "react";
-          setTodo("");
+import React, { useReducer } from "react";
+import { todoReducer } from "../reducers/todo";
+import "../styles/App.css";
+import { AddTodo } from "./AddTodo";
+import { Todo } from "./Todo";
+const App = () => {
+  const [state, dispatch] = useReducer(todoReducer, []);
 
-        }}
-const AddTodo = ({ dispatch }) => {
-
-      >
-  const [todo, setTodo] = useState("");
-        <input
   return (
-          id="todo-input"
-    <>
-          type="text"
-      <form
-          value={todo}
-        id="todo-form"
-          onChange={(e) => setTodo(e.target.value)}
-        onSubmit={(e) => {
+    <div id="main">
+      <AddTodo dispatch={dispatch} />
+      {state.map((task) => (
+ 
+        <Todo
+          key={task.id}
+          id={task.id}
+          value={task.title}
+          dispatch={dispatch}
         />
-          e.preventDefault();
-        <button type="submit">Add</button>
-          dispatch({ type: "add_todo", payload: { title: todo } });
-      </form>
-          setTodo("");
-    </>
-        }}
-  );
-      >
-};
-        <input
 
-          id="todo-input"
-export { AddTodo };
-          type="text"
-          value={todo}
-          onChange={(e) => setTodo(e.target.value)}
-        />
-        <button type="submit">Add</button>
-      </form>
-    </>
+      ))}
+    </div>
   );
 };
 
-export { AddTodo };
+export default App;
